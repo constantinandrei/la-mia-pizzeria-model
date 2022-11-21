5fc384cb974c646e9f7e13d16c298653921f5ba5
Constantin Andrei Todirascu <andrei.todirascu@gmail.com>
2022-11-21 13:36:58 +0100

    impostato path, controller e vista

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data;
+
+namespace la_mia_pizzeria_static.Controllers
+{
+    public class PizzaController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Todi Pizza";
+            return View();
+        }
+    }
+}
